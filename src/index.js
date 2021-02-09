a6d32c9b237f3c2c4cee6ea371fff632916f1040
theflyingpinata <42786585+theflyingpinata@users.noreply.github.com>
2021-02-09 15:53:02 -0800

    Dev Tools

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log("First web service starting up ...");
+console.log('First web service starting up ...');
 
 // 1 - pull in the HTTP server module
 const http = require('http');
@@ -22,64 +22,57 @@ const errorPage = `
     </body>
 </html>`;
 
-
-
-const jokes =  [
-    {"q":"What do you call a very small valentine?","a":"A valen-tiny!!!"},
-    {"q":"What did the dog say when he rubbed his tail on the sandpaper?","a":"Ruff, Ruff!!!"},
-    {"q":"Why don't sharks like to eat clowns?","a":"Because they taste funny!!!"},
-    {"q":"What did the boy cat say to the girl cat?","a":"You're Purr-fect!!!"},
-    {"q":"What is a frog's favorite outdoor sport?","a":"Fly Fishing!!!"},
-    {"q":"I hate jokes about German sausages.","a":"Theyre the wurst!!!"},
-    {"q":"Did you hear about the cheese factory that exploded in France?","a":"There was nothing left but de Brie!!!"},
-    {"q":"Our wedding was so beautiful ","a":"Even the cake was in tiers!!!"},
-    {"q":"Is this pool safe for diving?","a":"It deep ends!!!"},
-    {"q":"Dad, can you put my shoes on?","a":"I dont think theyll fit me!!!"},
-    {"q":"Can February March?","a":"No, but April May!!!"},
-    {"q":"What lies at the bottom of the ocean and twitches?","a":"A nervous wreck!!!"},
-    {"q":"Im reading a book on the history of glue.","a":"I just cant seem to put it down!!!"},
-    {"q":"Dad, can you put the cat out?","a":"I didnt know it was on fire!!!"},
-    {"q":"What did the ocean say to the sailboat?","a":"Nothing, it just waved!!!"},
-    {"q":"What do you get when you cross a snowman with a vampire?","a":"Frostbite!!!"}
+const jokes = [
+  { q: 'What do you call a very small valentine?', a: 'A valen-tiny!!!' },
+  { q: 'What did the dog say when he rubbed his tail on the sandpaper?', a: 'Ruff, Ruff!!!' },
+  { q: "Why don't sharks like to eat clowns?", a: 'Because they taste funny!!!' },
+  { q: 'What did the boy cat say to the girl cat?', a: "You're Purr-fect!!!" },
+  { q: "What is a frog's favorite outdoor sport?", a: 'Fly Fishing!!!' },
+  { q: 'I hate jokes about German sausages.', a: 'Theyre the wurst!!!' },
+  { q: 'Did you hear about the cheese factory that exploded in France?', a: 'There was nothing left but de Brie!!!' },
+  { q: 'Our wedding was so beautiful ', a: 'Even the cake was in tiers!!!' },
+  { q: 'Is this pool safe for diving?', a: 'It deep ends!!!' },
+  { q: 'Dad, can you put my shoes on?', a: 'I dont think theyll fit me!!!' },
+  { q: 'Can February March?', a: 'No, but April May!!!' },
+  { q: 'What lies at the bottom of the ocean and twitches?', a: 'A nervous wreck!!!' },
+  { q: 'Im reading a book on the history of glue.', a: 'I just cant seem to put it down!!!' },
+  { q: 'Dad, can you put the cat out?', a: 'I didnt know it was on fire!!!' },
+  { q: 'What did the ocean say to the sailboat?', a: 'Nothing, it just waved!!!' },
+  { q: 'What do you get when you cross a snowman with a vampire?', a: 'Frostbite!!!!!' },
 ];
 const getRandomJoke = () => {
-    let numJokes = jokes.length;
-    let randomNum = parseInt(Math.random() * numJokes);
-    let randomJoke = jokes[randomNum];
-    const responseObj = {
-        "q": randomJoke['q'],
-        "a": randomJoke['a']
-    }
-    return JSON.stringify(responseObj);
+  const numJokes = jokes.length;
+  const randomNum = parseInt(Math.random() * numJokes, 10);
+  const randomJoke = jokes[randomNum];
+  const responseObj = {
+    q: randomJoke.q,
+    a: randomJoke.a,
+  };
+  return JSON.stringify(responseObj);
 };
-
 
 // 7 - this is the function that will be called every time a client request comes in
 // this time we will look at the `pathname`, and send back the appropriate page
 // note that in this course we'll be using arrow functions 100% of the time in our server-side code
 const onRequest = (request, response) => {
-    //console.log(request.headers);
-    const parsedUrl = url.parse(request.url);
-    const pathname = parsedUrl.pathname;
-    //console.log("parsedUrl=", parsedUrl);
-    //console.log("pathname=", pathname);
+  // console.log(request.headers);
+  const parsedUrl = url.parse(request.url);
+  const { pathname } = parsedUrl;
+  // console.log("parsedUrl=", parsedUrl);
+  // console.log("pathname=", pathname);
 
-
-   if(pathname == "/random-joke"){
-        response.writeHead(200, { 'Content-Type': 'application/json'});
-        response.write(getRandomJoke());
-        response.end();
-    }
-    else{
-        response.writeHead(404, {'Content-Type': 'text/html'});
-        response.write(errorPage);
-        response.end();
-    }
+  if (pathname === '/random-joke') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(getRandomJoke());
+    response.end();
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.write(errorPage);
+    response.end();
+  }
 };
-
 
 // 8 - create the server, hook up the request handling function, and start listening on `port`
 http.createServer(onRequest).listen(port);
-
 
 console.log(`Listening on 127.0.0.1: ${port}`);
